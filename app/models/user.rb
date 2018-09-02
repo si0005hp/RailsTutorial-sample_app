5c3aa18965215fea399ed9e045c62a80fa308d05
si0005hp <si0005hp@gmail.com>
2018-09-03 01:55:37 +0800

    Chap11 11.1.2

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token
+  attr_accessor :remember_token, :activation_token
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
@@ -7,7 +7,8 @@ class User < ApplicationRecord
   validates :email, presence: true, length: {maximum: 255},
             format: {with: VALID_EMAIL_REGEX},
             uniqueness: {case_sensitive: false}
-  before_save {self.email = email.downcase}
+  before_save :downcase_email
+  before_create :create_activation_digest
   has_secure_password
   validates :password, presence: true, length: {minimum: 6}, allow_nil: true
 
@@ -33,5 +34,14 @@ class User < ApplicationRecord
 
   def forget
     update_attribute(:remember_digest, nil)
+  end
+
+  def downcase_email
+    self.email = email.downcase
+  end
+
+  def create_activation_digest
+    self.activation_token = User.new_token
+    self.activation_digest = User.digest(activation_token)
   end
 end
