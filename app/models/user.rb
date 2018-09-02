3accbb9635af319056cd842030fea030ad7d37b4
si0005hp <si0005hp@gmail.com>
2018-09-02 17:48:24 +0800

    Chap8 8.2.4

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,10 @@ class User < ApplicationRecord
   before_save {self.email = email.downcase}
   has_secure_password
   validates :password, presence: true, length: {minimum: 6}
+
+  def User.digest(str)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+               BCrypt::Engine.cost
+    BCrypt::Password.create(str, cost: cost)
+  end
 end
