6f577a6ee6395bf6186dbb3210c4ba6e5d1b697c
si0005hp <si0005hp@gmail.com>
2018-09-02 19:15:45 +0800

    Chap9 9.1.3

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -29,4 +29,16 @@ module SessionsHelper
     cookies.permanent.signed[:user_id] = user.id
     cookies.permanent[:remember_token] = user.remember_token
   end
+
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+
+  def log_out
+    forget(current_user)
+    session.delete(:user_id)
+    @current_user = nil
+  end
 end
