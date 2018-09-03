d29de09bbffef7f71714614d6c06a514a6b23424
si0005hp <si0005hp@gmail.com>
2018-09-04 00:34:56 +0800

    Chap12 12.2.1

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -6,8 +6,7 @@ class UserMailer < ApplicationMailer
   end
 
   def password_reset(user)
-    @greeting = "Hi"
-
-    mail to: "to@example.org"
+    @user = user
+    mail to: user.email, subject: "Password reset"
   end
 end
