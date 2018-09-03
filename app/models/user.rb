47fd1b85ef930daac74689c1447c9c262512da91
si0005hp <si0005hp@gmail.com>
2018-09-04 02:10:07 +0800

    Chap13 13.1.3

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :microposts
   attr_accessor :remember_token, :activation_token, :reset_token
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
