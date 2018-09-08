c7fef2cc59394b38e7072cf11d24b95f443c0299
si0005hp <si0005hp@gmail.com>
2018-09-09 00:55:55 +0800

    Chap13 13.3.5

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -16,7 +16,7 @@ class MicropostsController < ApplicationController
   def destroy
     @micropost.destroy
     flash[:success] = "Micropost deleted"
-    redirect_to request.referrer || root_urls
+    redirect_to request.referrer || root_url
   end
 
   private
